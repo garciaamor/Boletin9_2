f7a74e222dd198a10778ebc4ae68f33e6bec1007
jgarciaamor <jgarciaamor@atanasoff01.danielcastelao.org>
2015-11-12 12:41:00 +0100

    Boletin 9

---
+++ src/boletin9_2/Boletin9_2.java
@@ -0,0 +1,11 @@
+package boletin9_2;
+
+public class Boletin9_2 {
+    
+    public static void main(String[] args) {
+        
+        Clase9_2 obx=new Clase9_2();
+        obx.calcular();
+    }
+    
+}
